101097d9a988babaa812435e0c2dbe6c8cb94d9e
Nokk3n <dylan.werelius@gmail.com>
2023-05-17 14:43:55 -0700

    Bug Fix

--- viewingpage.cpp
+++ viewingpage.cpp
@@ -299,30 +299,62 @@ void ViewingPage::sortByLeague(string whatLeague)
         DbHandler dbHandler(DATABASE_PATH,DATABASE_CONNECTION_NAME);
             if(dbHandler.open())
             {
+                if (whatLeague == "National")
+                {
+                    QSqlQuery query1("SELECT Team_Name, Stadium_Name, Seat_Capacity, Location, Playing_Surface, League,"
+                                                     "Date_Opened, Distance_to_Center_Field, Ballpark_Typology, Roof_Type FROM MLB_Information ORDER BY STADIUM_NAME");
+
+                    while(query1.next())
+                    {
+                        string teamName = query1.value(0).toString().toStdString();
+                        string stadiumName = query1.value(1).toString().toStdString();
+                        int seatCapacity = query1.value(2).toInt();
+                        string location = query1.value(3).toString().toStdString();
+                        string playingSurface = query1.value(4).toString().toStdString();
+                        string league = query1.value(5).toString().toStdString();
+                        int dateOpened = query1.value(6).toInt();
+                        int distanceToCenter = query1.value(7).toInt();
+                        string ballparkTypology = query1.value(8).toString().toStdString();
+                        string roofType = query1.value(9).toString().toStdString();
+
+                        // Only add if league == the specific league in question
+                        if (league == whatLeague)
+                        {
+                            // Create Team Object
+                            Team t(teamName, stadiumName, seatCapacity, location, playingSurface, league, dateOpened, distanceToCenter, ballparkTypology, roofType);
+                            sortedLeague.insert(t);
+                        }
+                    }
+                }
+                else
+                {
                 QSqlQuery query1("SELECT Team_Name, Stadium_Name, Seat_Capacity, Location, Playing_Surface, League,"
                                  "Date_Opened, Distance_to_Center_Field, Ballpark_Typology, Roof_Type FROM MLB_Information ORDER BY TEAM_NAME");
 
-                            while(query1.next())
-                            {
-                                string teamName = query1.value(0).toString().toStdString();
-                                string stadiumName = query1.value(1).toString().toStdString();
-                                int seatCapacity = query1.value(2).toInt();
-                                string location = query1.value(3).toString().toStdString();
-                                string playingSurface = query1.value(4).toString().toStdString();
-                                string league = query1.value(5).toString().toStdString();
-                                int dateOpened = query1.value(6).toInt();
-                                int distanceToCenter = query1.value(7).toInt();
-                                string ballparkTypology = query1.value(8).toString().toStdString();
-                                string roofType = query1.value(9).toString().toStdString();
+                while(query1.next())
+                {
+                    string teamName = query1.value(0).toString().toStdString();
+                    string stadiumName = query1.value(1).toString().toStdString();
+                    int seatCapacity = query1.value(2).toInt();
+                    string location = query1.value(3).toString().toStdString();
+                    string playingSurface = query1.value(4).toString().toStdString();
+                    string league = query1.value(5).toString().toStdString();
+                    int dateOpened = query1.value(6).toInt();
+                    int distanceToCenter = query1.value(7).toInt();
+                    string ballparkTypology = query1.value(8).toString().toStdString();
+                    string roofType = query1.value(9).toString().toStdString();
 
-                                // Only add if league == the specific league in question
-                                if (league == whatLeague)
-                                {
-                                    // Create Team Object
-                                    Team t(teamName, stadiumName, seatCapacity, location, playingSurface, league, dateOpened, distanceToCenter, ballparkTypology, roofType);
-                                    sortedLeague.insert(t);
-                                }
-                            }
+                    // Only add if league == the specific league in question
+                    if (league == whatLeague)
+                    {
+                        // Create Team Object
+                        Team t(teamName, stadiumName, seatCapacity, location, playingSurface, league, dateOpened, distanceToCenter, ballparkTypology, roofType);
+                        sortedLeague.insert(t);
+                    }
+                }
+                }
+
+
 
             }
             else
